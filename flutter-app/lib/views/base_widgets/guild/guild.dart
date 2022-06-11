c8cf7d387557f3403da6b4f002f02ab13ecc455d
yccyennhi <ycc.yennhi@gmail.com>
2022-06-11 17:26:30 +0700

    feat: overview guild

---
+++ flutter-app/lib/views/base_widgets/guild/guild.dart
@@ -0,0 +1,56 @@
+import 'package:book_net/configs/color_configs.dart';
+import 'package:book_net/configs/style_configs.dart';
+import 'package:book_net/configs/text_configs.dart';
+import 'package:book_net/dto/guild/guild_dto.dart';
+import 'package:book_net/views/base_widgets/image/image.dart';
+import 'package:book_net/views/home_screen/detail_book_screen/detail_book_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Guild extends StatelessWidget {
+  const Guild({super.key, required this.guild, this.progress = -1});
+  final GuildDto guild;
+  final double progress;
+  @override
+  Widget build(BuildContext context) {
+    onTap() {
+      print('object' + guild.name);
+      Navigator.pushNamed(context, DetailBookScreen.id, arguments: guild);
+    }
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 220.h,
+        padding: EdgeInsets.only(right: 8.h),
+        decoration: const BoxDecoration(color: AppColors.whiteColor),
+        child: SizedBox(
+          width: 140.h,
+          height: 220.h,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomImage(
+                  imageUrl: guild.imageUrl,
+                  height: 140,
+                  width: 140,
+                  radius: AppStyles.radiusXss,
+                ),
+                Text(guild.name, style: TextConfigs.bold16),
+                SizedBox(
+                  height: 4.h,
+                ),
+                Text(guild.descrition,
+                    style: TextConfigs.regular12
+                        .copyWith(color: AppColors.darkGrayColor)),
+                SizedBox(
+                  height: 8.h,
+                ),
+              ]),
+        ),
+      ),
+    );
+  }
+}
--- flutter-app/lib/views/home_screen/guild_screen/guild_screen.dart
+++ flutter-app/lib/views/home_screen/guild_screen/guild_screen.dart
@@ -1,6 +1,11 @@
+import 'package:book_net/configs/color_configs.dart';
+import 'package:book_net/configs/style_configs.dart';
+import 'package:book_net/configs/text_configs.dart';
+import 'package:book_net/dto/news/base_news_dto.dart';
+import 'package:book_net/views/base_widgets/bar/bar.dart';
+import 'package:book_net/views/base_widgets/guild/guild.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class GuildScreen extends StatelessWidget {
   static const id = "GuildScreen";
@@ -8,8 +13,62 @@ class GuildScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('GuildScreen'),
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      appBar: PreferredSize(
+        preferredSize: Size(double.infinity, AppStyles.appBarHeight),
+        child: const Bars(
+          title: 'Guild',
+          leading: SizedBox(),
+        ),
+      ),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: EdgeInsets.all(24.h),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Downloaded', style: TextConfigs.bold18),
+            SizedBox(
+              height: 16.h,
+            ),
+            SizedBox(
+              height: 200.h,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: listGuild.length,
+                  itemBuilder: (context, i) {
+                    return Guild(
+                      guild: listGuild[i],
+                    );
+                  }),
+            ),
+            SizedBox(
+              height: 32.h,
+            ),
+            Text('Maybe you like', style: TextConfigs.bold18),
+            SizedBox(
+              height: 16.h,
+            ),
+            GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisExtent: 250,
+                ),
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: listGuild.length,
+                itemBuilder: (context, i) {
+                  return SizedBox(
+                    child: Guild(
+                      guild: listGuild[i],
+                    ),
+                  );
+                }),
+          ],
+        ),
+      )),
     );
   }
 }
